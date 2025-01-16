633f72fa942a663d0e0507adec8dd8d316c85135
Varsha MM <varsha@inflexiontechfze.com>
2025-01-16 07:19:48 +0000

    BTP PROJECT @Varsha

--- project1/webapp/controller/App.controller.js
+++ project1/webapp/controller/App.controller.js
@@ -1,10 +1,20 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/UIComponent"
+], function (Controller, UIComponent) {
   "use strict";
 
-  return BaseController.extend("project1.controller.App", {
-      onInit() {
+  return Controller.extend("project1.controller.App", {
+      onInit: function () {
+          // Application initialization logic
+      },
+
+      getRouter: function () {
+          return UIComponent.getRouterFor(this);
+      },
+
+      onNavBack: function () {
+          window.history.go(-1);
       }
   });
 });
--- project1/webapp/controller/Login.controller.js
+++ project1/webapp/controller/Login.controller.js
@@ -4,65 +4,65 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel"
 ], function(Controller, MessageBox, JSONModel) {
     "use strict";
-
+ 
     return Controller.extend("project1.controller.Login", {
         onInit: function() {
-            // Initialize the model for binding
             var oModel = new JSONModel({
                 username: "",
                 password: ""
             });
             this.getView().setModel(oModel, "loginModel");
-            
-            // Add console log to verify initialization
+           
+           
             console.log("Login Controller initialized");
         },
-
-        // Toggle password visibility
+ 
         onShowPassword: function(oEvent) {
-            var oSource = oEvent.getSource(); // Button clicked to toggle password visibility
-            var oPasswordField = this.getView().byId("passwordInput"); // The password input field
-            
-            // Toggle between showing password (Text type) and hiding it (Password type)
+            var oSource = oEvent.getSource(); 
+            var oPasswordField = this.getView().byId("passwordInput");
+           
+           
             if (oPasswordField.getType() === "Password") {
-                oPasswordField.setType("Text"); // Show the password
-                oSource.setIcon("sap-icon://hide"); // Change icon to 'hide'
+                oPasswordField.setType("Text"); 
+                oSource.setIcon("sap-icon://hide"); 
             } else {
-                oPasswordField.setType("Password"); // Hide the password
-                oSource.setIcon("sap-icon://show"); // Change icon to 'show'
+                oPasswordField.setType("Password"); 
+                oSource.setIcon("sap-icon://show"); 
             }
         },
-        onLoginPress: function() {
+
+        onLoginPress: async function() {
             var username = this.getView().byId("userInput").getValue();
             var password = this.getView().byId("passwordInput").getValue();
-         
+            
             sap.ui.core.BusyIndicator.show(0);
         
-            fetch('http://localhost:8081/api/users/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    username: username,
-                    password: password
-                })
-            })
-            .then(response => {
+            try {
+                var response = await fetch('http://localhost:8080/api/validateLogin', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    },
+                    mode: 'cors',
+                    body: JSON.stringify({
+                        username: username,
+                        password: password
+                    })
+                });
+        
                 console.log("Response status:", response.status);
-                return response.json();
-            })
-            .then(data => {
+                if (!response.ok) {
+                    throw new Error("Failed with status: " + response.status);
+                }
+        
+                var data = await response.json();
                 console.log("Login response:", data);
                 sap.ui.core.BusyIndicator.hide();
         
-                if (data.success) {
-                    // Store user data in model
+                if (datad.success) {
                     var userModel = new JSONModel({
-                        username: data.user.username,  
-                        //typically an object containing the response data from a backend API or server request. 
-                        // refers to a property of the data object, which is expected to contain an object representing user-related information.
-                        // This is accessing the username property of the user object inside data. It is likely a string that represents the username of the user
+                        username: data.user.username,
                         status: data.user.status
                     });
                     this.getOwnerComponent().setModel(userModel, "userModel");
@@ -78,29 +78,15 @@ sap.ui.define([
                         title: "Login Failed"
                     });
                 }
-            })
-            .catch(error => {
+            } catch (error) {
                 console.error("Login error:", error);
                 sap.ui.core.BusyIndicator.hide();
         
                 MessageBox.error("An error occurred during login. Please try again.", {
                     title: "Login Failed"
                 });
-            });
-        },
-
-        onUsernameChange: function(oEvent) {
-            var input = oEvent.getSource();
-            if (input.getValue()) {
-                input.setValueState("None");
             }
         },
-
-        onPasswordChange: function(oEvent) {
-            var input = oEvent.getSource();
-            if (input.getValue()) {
-                input.setValueState("None");
-            }
-        }
+        
     });
 });
